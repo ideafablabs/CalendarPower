d3c70b443d658c2af51fdd4555cc8219d02f88d6
Scotty Franzyshen <sfranzyshen@gmail.com>
2022-02-28 09:00:07 -0800

    formatting

--- calendar_power/secret.h
+++ calendar_power/secret.h
@@ -1,7 +1,7 @@
-#define STASSID "enter ssid"            // "MyWiFi"
-#define STAPSK  "enter password"        // "pennygetyourownwifi"
+#define STASSID   "enter ssid"            // "MyWiFi"
+#define STAPSK    "enter password"        // "pennygetyourownwifi"
 
-#define HOSTNAME  "enter host name"     // "calendar.google.com"
-#define HOSTPRINT "enter finger print"  // "8e 4a b0 91 50 92 3e 08 a2 cb 65 fd c1 df 57 4f f5 ed 90 bd"
-#define HOSTURL   "enter host url"      // "/calendar/ical/username%40gmail.com/public/basic.ics"
+#define HOSTNAME  "enter host name"       // "calendar.google.com"
+#define HOSTPRINT "enter finger print"    // "8e 4a b0 91 50 92 3e 08 a2 cb 65 fd c1 df 57 4f f5 ed 90 bd"
+#define HOSTURL   "enter host url"        // "/calendar/ical/username%40gmail.com/public/basic.ics"
 
